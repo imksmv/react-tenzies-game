641d0a267c74fad1ca700aa36da9b9c06c756198
imksmv <98290303+imksmv@users.noreply.github.com>
2022-06-22 21:00:15 +0100

    Create Tenzies game UI + dark theme toggle

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Main from "./components/Main";
 import "./App.css";
 
 const App = () => {
+    const [darkMode, setDarkMode] = React.useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode((prevMode) => !prevMode);
+    };
+
     return (
         <div className="wrapper">
-            <Main />
+            <Main darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         </div>
     );
 };
